da3049111eb1a8012321840dcacdf92c91b217ac
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-07-10 14:20:21 +0200

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,54 +1,51 @@
 {
-  description = "Narcos";
+  description = "NARC CLI";
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 self.lastModifiedDate;
-
-      # System types to support.
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-      # Remove .gits etc from the build inputs
-      src = nixpkgs.lib.cleanSource ./.;
-
-    in {
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          # The default package for 'nix build'. This makes sense if the
-          # flake provides only one package or there is a clear "main"
-          # package.
-          default = pkgs.buildGoModule {
-            pname = "narcos";
-            inherit version;
-            inherit src;
-
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remeber to bump this hash when your dependencies change.
-            #vendorHash = pkgs.lib.fakeSha256;
-
-            vendorHash = "sha256-H0QWd981oUdft2vgsa4KvBv/uWR1+C1rf0e7KLBhRs8";
-          };
-        });
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    version = builtins.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101");
+    goOverlay = final: prev: {
+      go = prev.go.overrideAttrs (old: {
+        version = "1.22.5";
+        src = prev.fetchurl {
+          url = "https://go.dev/dl/go1.22.5.src.tar.gz";
+          hash = "sha256-rJxyPyJJaa7mJLw0/TTJ4T8qIS11xxyAfeZEu0bhEvY=";
+        };
+      });
     };
+    withSystem = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    withPkgs = callback:
+      withSystem (
+        system:
+          callback
+          (import nixpkgs {
+            inherit system;
+            overlays = [goOverlay];
+          })
+      );
+  in {
+    packages = withPkgs (
+      pkgs: rec {
+        narc = pkgs.buildGoModule {
+          pname = "narc";
+          inherit version;
+          src = ./.;
+          vendorHash = "sha256-AgRQO3h7Atq4lnieTBohzrwrw0lRcbQi2cvpeol3owM=";
+        };
+        default = narc;
+      }
+    );
+
+    devShells = withPkgs (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [go gopls gotools go-tools];
+      };
+    });
+
+    formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+  };
 }
