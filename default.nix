2e4c1cee4d0d00bfaf35376c88098144f44c405f
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-10-23 16:03:53 +0200

    nix build: remove useless step

--- default.nix
+++ default.nix
@@ -6,10 +6,8 @@
 buildGoApplication {
   pname = "narcos";
   version = version;
+  subPackages = ["cmd/narc"];
   pwd = ./.;
   src = ./.;
   modules = ./gomod2nix.toml;
-  postInstall = ''
-    [[ -f $out/bin/narcos ]] && mv $out/bin/narcos $out/bin/narc
-  '';
 }
