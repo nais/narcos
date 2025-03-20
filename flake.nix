df3ba6811d5297bb5809ca39ed78e1ea6534153f
x10an14-nav <christian.chavez@nav.no>
2025-03-20 12:11:42 +0100

    flake: Do the same as in nais/cli
    See https://github.com/nais/cli/commit/6326b1fbfb079a71dcd489e562eae57fecc94869

--- flake.nix
+++ flake.nix
@@ -1,48 +1,63 @@
 {
   description = "narcos cli - for nais admins";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, }:
-    let
-      version = builtins.substring 0 8
-        (self.lastModifiedDate or self.lastModified or "19700101");
-      # goOverlay = final: prev: {
-      #   go = prev.go.overrideAttrs (old: {
-      #     version = "1.23.2";
-      #     src = prev.fetchurl {
-      #       url = "https://go.dev/dl/go1.23.2.src.tar.gz";
-      #       hash = "sha256-NpMBYqk99BfZC9IsbhTa/0cFuqwrAkGO3aZxzfqc0H8=";
-      #     };
-      #   });
-      # };
-      withSystem = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      withPkgs = callback:
-        withSystem (system:
-          callback (import nixpkgs {
-            inherit system;
-            # overlays = [goOverlay];
-          }));
-    in {
-      packages = withPkgs (pkgs: rec {
-        narc = pkgs.buildGoModule {
-          pname = "narc";
-          inherit version;
-          src = ./.;
-          vendorHash = "sha256-4yAMLrPePI9FxvybN0AUNXn5mFTmkiooqVLUFXp3H8c=";
-        };
-        default = narc;
-      });
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-utils.lib.eachSystem
+      [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ]
+      (
+        system:
+        let
+          version = builtins.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101");
+          pkgs = import inputs.nixpkgs {
+            localSystem = { inherit system; };
+            overlays = [
+              (
+                final: prev:
+                let
+                  version = "1.23.2";
+                  newerGoVersion = prev.go.overrideAttrs (old: {
+                    inherit version;
+                    src = prev.fetchurl {
+                      url = "https://go.dev/dl/go${version}.src.tar.gz";
+                      hash = "sha256-NpMBYqk99BfZC9IsbhTa/0cFuqwrAkGO3aZxzfqc0H8=";
+                    };
+                  });
+                  nixpkgsVersion = prev.go.version;
+                  newVersionNotInNixpkgs = -1 == builtins.compareVersions nixpkgsVersion version;
+                in
+                {
+                  go = if newVersionNotInNixpkgs then newerGoVersion else prev.go;
+                  buildGoModule = prev.buildGoModule.override { go = final.go; };
+                }
+              )
+            ];
+          };
+        in
+        {
+          packages = rec {
+            narc = pkgs.buildGoModule {
+              pname = "narc";
+              inherit version;
+              src = ./.;
+              vendorHash = "sha256-fDOJjZAAKHl006mIHYuvwP6z+RW0prcPQdU6f1JUEdo=";
+            };
+            default = narc;
+          };
 
-      devShells = withPkgs (pkgs: {
-        default = pkgs.mkShell { buildInputs = with pkgs; [ go ]; };
-      });
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              gotools
+              go-tools
+            ];
+          };
 
-      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
-    };
+          formatter = pkgs.nixfmt-rfc-style;
+        }
+      );
 }
