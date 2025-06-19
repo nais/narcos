5fb1591030d9fab9812c91db030be397779cbba0
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-06-19 15:02:10 +0200

    update script name in completion script

--- script/completions.sh
+++ script/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish powershell; do
-	go run main.go completion "$sh" >"completions/nais.$sh"
+	go run main.go completion "$sh" >"completions/narc.$sh"
 done
