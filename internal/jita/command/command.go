36ddb866578e74673bfc98277f5cbda2957fc4cb
Christer Edvartsen <christer.edvartsen@nav.no>
2025-06-19 19:02:11 +0200

    refactor: use heredoc for description text

--- internal/jita/command/command.go
+++ internal/jita/command/command.go
@@ -3,6 +3,7 @@ package command
 import (
 	"context"
 
+	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/nais/cli/pkg/cli"
 	"github.com/nais/narcos/internal/jita"
 	"github.com/nais/narcos/internal/jita/command/flag"
@@ -22,16 +23,14 @@ func Jita(rootFlags *root.Flags) *cli.Command {
 }
 
 func list(parentFlags *flag.JitaFlags) *cli.Command {
-	flags := &flag.ListFlags{
-		JitaFlags: parentFlags,
-	}
+	flags := &flag.ListFlags{JitaFlags: parentFlags}
 	return &cli.Command{
 		Name:  "list",
 		Title: "List active and potential privilege elevations",
 		Flags: flags,
 		Args: []cli.Argument{
 			{
-				Name:       "TENANT",
+				Name:       "tenant",
 				Repeatable: true,
 			},
 		},
@@ -42,14 +41,17 @@ func list(parentFlags *flag.JitaFlags) *cli.Command {
 }
 
 func grant(parentFlags *flag.JitaFlags) *cli.Command {
-	flags := &flag.GrantFlags{
-		JitaFlags: parentFlags,
-	}
+	flags := &flag.GrantFlags{JitaFlags: parentFlags}
 	return &cli.Command{
-		Name:        "grant",
-		Title:       "Elevate privileges for this tenant",
-		Description: "TENANT is one of the tenants given by `narc tenant list`\nENTITLEMENT is one the entitlements given by `narc jita list <TENANT>`\nDURATION is the amount of time you need privileges for, given as 0h0m\nREASON is a human-readable description of why you need to elevate privileges.",
-		Flags:       flags,
+		Name:  "grant",
+		Title: "Elevate privileges for this tenant.",
+		Description: heredoc.Doc(`
+			TENANT is one of the tenants given by "narc tenant list"
+			ENTITLEMENT is one the entitlements given by "narc jita list TENANT"
+			DURATION is the amount of time you need privileges for, given as "0h0m"
+			REASON is a human-readable description of why you need to elevate privileges.
+		`),
+		Flags: flags,
 		Args: []cli.Argument{
 			{
 				Name:     "tenant",
