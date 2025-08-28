c2675939f99d94785fed03ddf472d1fe1cc5535f
Christer Edvartsen <christer.edvartsen@nav.no>
2025-08-28 10:04:11 +0200

    build: add missing dependency

--- mise/tasks/build/deb.sh
+++ mise/tasks/build/deb.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #MISE description="Package the Narc CLI binary as a Debian package"
+#MISE depends=["build"]
 export ARCH=$GOARCH
 GOARCH="" go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package \
   --packager deb \
