e1a34aa1646c06138564907bd3b85e7fd48c6bbc
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2026-04-22 22:40:33 +0200

    feat(debug): add pod name autocompletion for debug command
    Lists running pods in the specified namespace when tab-completing
    the pod argument. Requires -n flag to be set for completions.

--- internal/debug/command/command.go
+++ internal/debug/command/command.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nais/naistrix"
 	"github.com/nais/narcos/internal/debug"
 	"github.com/nais/narcos/internal/debug/command/flag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Debug(globalFlags *naistrix.GlobalFlags) *naistrix.Command {
@@ -19,6 +20,34 @@ func Debug(globalFlags *naistrix.GlobalFlags) *naistrix.Command {
 		Flags: flags,
 		Args: []naistrix.Argument{
 			{Name: "pod"},
+		},
+		AutoCompleteFunc: func(ctx context.Context, args *naistrix.Arguments, _ string) ([]string, string) {
+			if args.Len() >= 1 {
+				return nil, ""
+			}
+
+			if flags.Namespace == "" {
+				return nil, "Specify namespace (-n) to get pod completions."
+			}
+
+			typedClient, _, _, err := debug.NewClients(flags.KubeContext)
+			if err != nil {
+				return nil, "Unable to create Kubernetes client for autocomplete."
+			}
+
+			pods, err := typedClient.CoreV1().Pods(flags.Namespace).List(ctx, metav1.ListOptions{
+				FieldSelector: "status.phase=Running",
+			})
+			if err != nil {
+				return nil, "Unable to list pods for autocomplete."
+			}
+
+			names := make([]string, 0, len(pods.Items))
+			for _, pod := range pods.Items {
+				names = append(names, pod.Name)
+			}
+
+			return names, "Choose the pod to debug."
 		},
 		ValidateFunc: func(_ context.Context, args *naistrix.Arguments) error {
 			if args.Get("pod") == "" {
